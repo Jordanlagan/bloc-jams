8d4aebae33e06a4f70b7e22e3f117cf8cbd71777
Jordan Lagan <jordanlagan000@gmail.com>
2017-08-23 13:27:52 -0400

    assignment 21: complete

--- scripts/album.js
+++ scripts/album.js
@@ -3,7 +3,7 @@ var createSongRow = function(songNumber, songName, songLength) {
   '<tr class="album-view-song-item">'
 + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
 + '  <td class="song-item-title">' + songName + '</td>'
-+ '  <td class="song-item-duration">' + songLength + '</td>'
++ '  <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
 + '</tr>'
 ;
 
@@ -94,6 +94,7 @@ var updateSeekBarWhileSongPlays = function() {
       var $seekBar = $('.seek-control .seek-bar');
 
       updateSeekPercentage($seekBar, seekBarFillRatio);
+      setCurrentTimeInPlayerBar(filterTimeCode(this.getTime()));
     });
   }
 };
@@ -174,6 +175,9 @@ var updatePlayerBarSong = function() {
   $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
 
   $('.main-controls .play-pause').html(playerBarPauseButton);
+  if (currentSoundFile) {
+    setTotalTimeInPlayerBar(filterTimeCode(currentSongFromAlbum.duration));
+  }
 };
 
 var nextSong = function() {
@@ -224,6 +228,25 @@ var previousSong = function() {
 
 var getSongNumberCell = function(number) {
    return $('.song-item-number[data-song-number="' + number + '"]');
+};
+
+var setCurrentTimeInPlayerBar = function(currentTime) {
+  var $time = $('.seek-control .current-time');
+  $time.text(currentTime);
+};
+
+var setTotalTimeInPlayerBar = function(totalTime) {
+  var $total = $('.seek-control .total-time');
+  $total.text(totalTime);
+};
+
+var filterTimeCode = function(timeInSeconds) {
+  var totalTime = parseFloat(timeInSeconds);
+  var minutes = Math.floor(totalTime / 60);
+  var seconds = Math.floor(totalTime % 60);
+
+  if (seconds<10) {seconds="0"+seconds};
+  return minutes + ":" + seconds;
 };
 
 //Album button templates
